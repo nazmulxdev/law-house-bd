949e4c61a1f1f84a3ee9f8fce4b971a208693c14
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-04-22 03:34:38 +0600

    added lawyer details page

--- src/Pages/SingleLawyerDetails/SingleLawyerDetails.jsx
+++ src/Pages/SingleLawyerDetails/SingleLawyerDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
+import { PiWarningOctagonDuotone } from "react-icons/pi";
 
 const SingleLawyerDetails = ({ params }) => {
   const allLawyersDetails = useLoaderData();
@@ -10,7 +11,6 @@ const SingleLawyerDetails = ({ params }) => {
   );
 
   const {
-    id,
     name,
     availability_days,
     image,
@@ -18,9 +18,22 @@ const SingleLawyerDetails = ({ params }) => {
     years_of_experience,
     consultation_fee,
   } = lawyerDetail;
+
+  const daysName = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = daysName[new Date().getDay()];
+
+  const isAvailableToday = availability_days.includes(today);
   return (
     <div>
-      <div className="max-w-screen-2xl mx-auto p-16 rounded-2xl bg-[#0F0F0F0D] mb-8 text-center">
+      <div className="max-w-screen-2xl mx-auto p-16 rounded-2xl bg-[#0F0F0F0D] mb-8 text-center border-2 border-[#C4C4C4]">
         <h1 className=" text-3xl font-bold text-black pb-4">
           Lawyer’s Profile Details
         </h1>
@@ -34,7 +47,7 @@ const SingleLawyerDetails = ({ params }) => {
 
       <div className="">
         <div
-          className="max-w-screen-2xl mx-auto p-8  bg-[#0F0F0F0D] mb-8 border-2 border-[#C4C4C4] rounded-2xl grid grid-cols-3
+          className="max-w-screen-2xl mx-auto p-8 mb-8 border-2 border-[#C4C4C4] rounded-2xl grid grid-cols-3
      items-center gap-16"
         >
           <div className="col-span-1 rounded-[0.75rem]">
@@ -83,6 +96,43 @@ const SingleLawyerDetails = ({ params }) => {
                 ${consultation_fee}
               </span>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-screen-2xl mx-auto p-8 rounded-2xl border-2 border-[#C4C4C4] mb-8 text-center">
+        <h1 className="border-b-2 border-dashed border-[#C4C4C4] text-3xl font-bold text-black pb-4">
+          Book an Appointment
+        </h1>
+        <div className="border-b-2 border-dashed border-[#0F0F0F26] flex items-center justify-between">
+          <p className="text-[#141414] mt-4 text-xl font-semibold mb-2">
+            Availability
+          </p>
+          <p className="text-[#141414] mt-4 text-xl font-semibold mb-2">
+            {isAvailableToday ? (
+              <div className="bg-[#09982F10] rounded-full px-4 py-2">
+                <p className="text-[#09982F]">Lawyer Available Today</p>
+              </div>
+            ) : (
+              <div className="bg-[#FFA00033] rounded-full px-4 py-2">
+                <p className="text-[#FFA000]">Lawyer not Available Today</p>
+              </div>
+            )}
+          </p>
+        </div>
+        <div>
+          <div className="bg-[#FFA00033] rounded-full px-4 py-4 flex items-center gap-4 justify-center mt-8 mb-4">
+            <PiWarningOctagonDuotone className="text-[#FFA000] font-medium text-lg" />
+            <p className="text-[#FFA000] font-medium text-lg">
+              Lawyer not Due to high patient volume, we are currently accepting
+              appointments for today only. We appreciate your understanding and
+              cooperation.
+            </p>
+          </div>
+          <div className="px-4">
+            <button className="btn bg-[#0EA106] text-white font-medium text-xl mulish px-8 py-8 rounded-full  w-full">
+              Book Appointment Now
+            </button>
           </div>
         </div>
       </div>
